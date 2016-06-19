e6041b56641ac444973a414d4d0e65200163ac12
Elisha Marshall <iamelimars@yahoo.com>
2016-06-19 00:47:48 -0400

    Initial Commit

---
+++ Vision Board/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Vision Board
+//
+//  Created by iMac on 6/19/16.
+//  Copyright © 2016 Marshall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Vision Board/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Vision Board
+//
+//  Created by iMac on 6/19/16.
+//  Copyright © 2016 Marshall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
